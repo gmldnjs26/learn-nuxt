41a0d3b5561cbc3cbee19c1fd03d9aa78e9241d2
gmldnjs26 <imleesky265@gmail.com>
2021-09-20 19:27:24 +0900

    study: error page, layout

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+  <h1>{{ $route.name }}</h1>
+  <Nuxt/>
+</div>
+</template>
---
+++ pages/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    main이다
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
