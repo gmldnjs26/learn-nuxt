da12c3745527065abdd71ba47ec4a0d7508f67c2
gmldnjs26 <imleesky265@gmail.com>
2021-09-27 22:01:02 +0900

    asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,61 @@
 <template>
-  <Tutorial />
+  <div class="app">
+    <main>
+      <ul>
+        <li v-for="item in products" :key="item.id" class="item flex">
+          <img class="product-image" :src="item.imageUrl" alt="" />
+          <p>{{ item.name }}</p>
+          <span>{{ item.price }}</span>
+        </li>
+      </ul>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+// import { debounce } from 'lodash'
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => {
+      return { ...item, imageUrl: `${item.imageUrl}?random=${Math.random()}` }
+    })
+    return { products }
+  },
+}
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
--- pages/main.vue
+++ pages/main.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    main이다asdasdasddsmain이다asdasdasddsmain이다asdasdasddsmain이다asdasdasddsmain이다asdasdasddsmain이다asdasdasdds
+    <h1>메인 페이지</h1>
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+        </li>
+      </ul>
+    </div>
+    {{ products }}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  created() {},
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => {
+      return { ...item, imageUrl: `${item.imageUrl}?random=${Math.random()}` }
+    })
+    return { products }
+  },
 }
 </script>
 
