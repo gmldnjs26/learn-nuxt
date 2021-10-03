563806c4424bbbc577b16fc3ee18a76f364a94c8
gmldnjs26 <imleesky265@gmail.com>
2021-10-03 16:03:35 +0900

    feat: fetch carts add cartItem

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ const instance = axios.create({
   baseURL: 'http://localhost:3000',
 })
 
+// const products = axios.create({ // 이런식으로도 할 수 있다.
+//   baseURL: 'http://localhost:3000/products',
+// })
+
 function fetchProductById(id) {
   return instance.get(`/products/${id}`)
 }
@@ -16,4 +20,12 @@ function fetchProductsByKeyword(keyword) {
   })
 }
 
-export { fetchProductById, fetchProductsByKeyword }
+function createCartItem(cartItem) {
+  return instance.post('/carts', cartItem)
+}
+
+function fetchCartItems() {
+  return instance.get('/carts')
+}
+
+export { fetchProductById, fetchProductsByKeyword, createCartItem, fetchCartItems }
--- store/index.js
+++ store/index.js
@@ -1,9 +1,24 @@
+import { fetchCartItems } from '~/api'
+
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS' // 추후의 관리, 타입측면에서 이점이 있다?
+
 export const state = () => ({
   cartItems: [],
 })
 
 export const mutations = {
-  addCartItem(state, cartItems) {
-    state.cartItems.push(cartItems)
+  setCartItems(state, cartItems) {
+    state.cartItems = cartItems
+  },
+  addCartItem(state, cartItem) {
+    state.cartItems.push(cartItem)
+  },
+}
+
+export const actions = {
+  // 동적 키값할당
+  async [FETCH_CART_ITEMS]({ commit }) {
+    const response = await fetchCartItems()
+    commit('setCartItems', response.data)
   },
 }
